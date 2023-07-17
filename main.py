4f91fb1b08addcbdc9b3095eb6f0303eb9c7941c
19blacklowz <124819917+19blacklowz@users.noreply.github.com>
2023-07-17 20:37:18 +0000

    Basic Routes

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 #Book Club 2023
-#Testing
-#Test 2
-#Testing
-#Change
-#Change
+
+from flask import Flask, render_template, abort
+app = Flask(__name__)
+
+# basic route
+@app.route('/')
+def root():
+    return render_template("home.html", page_title='HOME')
+  
+@app.route('/members')
+def members():
+  return render_template('members.html', page_title= 'MEMBERs')
+
+@app.route('/all_books')
+def all_books():
+  return render_template('all_books.html', page_title= 'all_books')
+  
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
